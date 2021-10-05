11cbbda14c2cab20814d14c091f5e145a24d2eac
namancg <naman.c@perfios.com>
2021-10-05 13:20:06 +0530

    REFACTORED: code to calculate daily wage of employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,15 +1,23 @@
-//UC1 - to check the attendance of employee
-{
-    const IS_ABSENT=0
-    let empCheck=Math.floor(Math.random()*10)%2;
-    if(empCheck==IS_ABSENT)
-    {
-        console.log("EMPLOYEE IS ABSENT");
-        return;
-    }
-    else
-    {
-        console.log("EMPLOYEE IS PRESENT ");
-    }
+//UC2 - to calculate the wage
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOUR = 4;
+const FULL_TIME_HOUR = 8;
+const WAGE_PER_HOUR = 20;
 
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10 ) % 3;
+
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOUR;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOUR;
+        break;
+    default :
+    empHrs = 0;
 }
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee wage is :" + empWage)
