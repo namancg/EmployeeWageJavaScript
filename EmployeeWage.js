27bfa5a83172efb0792742858739f72fa5a8ed44
namancg <naman.c@perfios.com>
2021-10-05 13:49:07 +0530

    REFACTORED: code to calculate daily wage of employee for a month in max hours conditions using while

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,11 @@
-//UC4 - to calculate wage of a month using function
+//UC5 - to calculate wage of a month using while
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -19,9 +20,11 @@ function getWorkingHours(empCheck) {
     }
 }
 let totalEmpHrs = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+let toatlWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && toatlWorkingDays < NUM_OF_WORKING_DAYS) {
+    toatlWorkingDays ++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     totalEmpHrs += getWorkingHours(empCheck);
 }
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("EMPLOYEE HOURS IS : "+totalEmpHrs+" hr\nAND EMPLOYEE WAGE IS : " + empWage)
+console.log("EMPLOYEE HOURS IS : "+totalEmpHrs+" hr\nAND TOTAL WORKING DAYS : " + toatlWorkingDays + "\nAND EMPLOYEE WAGE IS : " + empWage)
