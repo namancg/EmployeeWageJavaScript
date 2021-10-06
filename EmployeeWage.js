12556994c525548def21eae2d8ad3c40bd17fdca
namancg <naman.c@perfios.com>
2021-10-06 17:18:27 +0530

    REFACTORED: code to store daily wage along with total wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,4 @@
-//UC5 - to calculate wage of a month using while
+//UC6 - to calculate total wage and daily wage
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOUR = 4;
@@ -19,12 +19,19 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
+function calculateWage(empHrs) {
+    return WAGE_PER_HOUR * empHrs;
+}
 let totalEmpHrs = 0;
 let toatlWorkingDays = 0;
+let empDailyWageArr = new Array();
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && toatlWorkingDays < NUM_OF_WORKING_DAYS) {
     toatlWorkingDays ++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calculateWage(empHrs))
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("EMPLOYEE HOURS IS : "+totalEmpHrs+" hr\nAND TOTAL WORKING DAYS : " + toatlWorkingDays + "\nAND EMPLOYEE WAGE IS : " + empWage)
+let totalEmpWage =calculateWage(totalEmpHrs);
+console.log("EMPLOYEE HOURS IS : "+totalEmpHrs+" hr\nAND TOTAL WORKING DAYS : " + toatlWorkingDays + "\nAND EMPLOYEE WAGE IS : " + totalEmpWage);
+console.log("DAILY EMPLOYEE WAGES ARE: "+empDailyWageArr);
