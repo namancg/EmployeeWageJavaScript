3b379b93651c3f7e0a2c50f48db9a79cebb5b7ab
namancg <naman.c@perfios.com>
2021-10-07 17:16:27 +0530

    REFACTORED: code to validate gender, employeeId, salary and start date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,8 +1,4 @@
 class EmployeePayrollData {
-    id;
-    salary;
-    gender;
-    startDate;
 
     constructor(...params) {
         this.id = params[0];
@@ -16,7 +12,37 @@ class EmployeePayrollData {
     set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
         if(nameRegex.test(name)) this._name = name;
-        else throw 'Name is incorrect';
+        else throw 'NAME IS INCORECT';
+    }
+
+    get id() {return this._id; }
+    set id(id) {
+        let idRegex = RegExp('^\d*[1-9]\d*$')
+        if(idRegex.test(id)) this._id = id;
+        else 
+            throw 'ID IS INCORECT';
+    }
+
+    get salary() {return this._salary; }
+    set salary(salary) {
+        let salaryRegex = RegExp('^[0-9.]+$')
+        if(salaryRegex.test(salary)) this._salary = salary;
+        else 
+            throw 'SALARY IS INCORECT';
+    }
+
+    get gender() {return this._gender; }
+    set gender(gender) {
+        let genderRegex = RegExp('^[MFmf]{1}$')
+        if(genderRegex.test(gender)) this._gender = gender;
+        else 
+            throw 'GENDER IS INCORECT';
+    }
+
+    get startDate() {return this._startDate; }
+    set startDate(startDate) {
+        if(new Date() < startDate) throw 'START DATE IS INCORECT';
+        else this._startDate = startDate;
     }
 
     
@@ -28,17 +54,11 @@ class EmployeePayrollData {
         return "id=" + this.id + ", name: "+this.name + ", salary: " + this.salary
         + ", gender: "+this.gender+", startDate: "+empDate;
     }
-} 
-
-let employeePayrollData  = new EmployeePayrollData(1, "Mark", 30000);
-employeePayrollData.id = 0;
-try {
-    employeePayrollData.name = 'Jef';
-    console.log("employeePayrollData: "+employeePayrollData.toString());
 }
-catch (e) {
+
+try {
+    let newEmployeePayrollData  = new EmployeePayrollData(1, "Terissa", 30000, 'm', new Date('2022-01-01'));
+    console.log("employeePayrollData: "+newEmployeePayrollData.toString());
+} catch (e) {
     console.error(e);
 }
-
-let newEmployeePayrollData  = new EmployeePayrollData(1, "Terissa", 30000, 'f', new Date());
-console.log("employeePayrollData: "+newEmployeePayrollData.toString());
